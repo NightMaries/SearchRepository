71bad3ac01c8e8ae31116b2c5386bea2dd9807d6
Kirill <ksadovnikov13@gmail.com>
2024-11-08 20:18:28 +0300

    .

--- SearchRepository.API/obj/Debug/net8.0/SearchRepository.API.AssemblyInfo.cs
+++ SearchRepository.API/obj/Debug/net8.0/SearchRepository.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchRepository.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810d23e54908e64ead6991d8506f1044ac29c6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae358f23551f0d7bcf5743f8b086d515e18a651")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchRepository.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchRepository.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
